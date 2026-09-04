23646465235e72604682a7efaa4823338a5ded98
audreypaulynneolaybar <audreypaulynne.olaybar@gmail.com>
2026-09-04 11:16:41 +0800

    added_str

--- TASK 5.py
+++ TASK 5.py
@@ -18,15 +18,15 @@ class Material (ABC):
 #subclasses
 
 class Metal(Material):
-    def material_category(self):
+    def material_category(self) -> str:
         return "Metallic material"
 
 class Plastic(Material):
-    def material_category(self):
+    def material_category(self) -> str:
         return "Polymeric material"
 
 class Composite(Material):
-    def material_category(self):
+    def material_category(self) -> str:
         return "Composite material"
 
 #Test the classes
